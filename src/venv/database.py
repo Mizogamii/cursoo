13447ff329be3139a48df29dfd86ea8a882cd1fc
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2023-10-14 20:37:39 -0300

    Update database.py

--- src/venv/database.py
+++ src/venv/database.py
@@ -1,5 +1,12 @@
 from pymongo.mongo_client import MongoClient
-from dotenv 
+from dotenv import load_dotenv
+import os
 class DataBase:
+    def __init__():
+        load_dotenv()
+
     def connect(self):
-        MongoClient()
+        client = MongoClient(os.getenv('DB_URI'))
+        collection = client['news']
+        return collection.news
+    
