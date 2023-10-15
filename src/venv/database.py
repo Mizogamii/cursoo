cc4212d0704464f65d88e0b557753fa5cde9dc3b
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2023-10-15 17:18:53 -0300

    j

--- src/venv/database.py
+++ src/venv/database.py
@@ -6,26 +6,32 @@ class DataBase:
     def __init__(self):
         load_dotenv()
         self.news = self.connect()
+        print("Init")
 
     def connect(self):
         client = MongoClient(os.getenv('DB_URI'))
         db = client['curso']
+        print("Connect")
         return db.news
         
     def insert(self, data: dict)-> dict | None:
         query = {'title': data['title']}
         resultado = self.news.find(query).sort('date', -1)
+
         if resultado is None:
             self.news.insert_one(data)
-            print(data)
+            print("O outro")
             return data
         else:
-            return None
+            news = data.copy()
+            print("Testando")
+            return news
         
 if __name__ == "__main__":
     db = DataBase()
     data = {'title': 'Assista ao trailer do filme "Priscilla', 'texto': 'Drama de Sofia Coppola, com Cailee Spaeny, Jacob Elordi, Ari Cohen e Dagmara Dominczyk.'}
     db.insert(data)
+    print("name___")
 
 """if __name__ == "__main__":
     db = DataBase()
